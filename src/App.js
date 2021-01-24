1dea9dac77b217d2e554efb6a940c3c3408d6fce
Thaina Carvalho <thaina.carvalho@driven.cx>
2021-01-24 14:59:23 -0300

    contato

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
                 <div className="quem-somos-title"><h2>Olá, somos a Jüssi</h2></div>
                 <div className="quem-somos-text">A Jüssi é uma agência integrante do grupo global WPP que vem há 10 anos consolidando o pensamento voltado para produtos e resolução de problemas. Nosso área dedicada exclusivamente para Produtos Digitais é organizada em 6 especialidades: Product Managamenet, User Experience Design, SEO, Tecnologia, Agile e User Behavior Analytics.</div>
                 <div className="quem-somos-button">
-                  <a href="https://jussi.com.br/about.html" target="_blank">Conheça a Jüssi</a>
+                  <a href="https://jussi.com.br/about.html" rel="noreferrer" target="_blank">Conheça a Jüssi</a>
                 </div>
               </div>
               <div className="quem-somos--right">
@@ -51,18 +51,29 @@ function App() {
           </div>
         </section>
         <section className="contato">
-
+          <div className="container">
+            <h2>Essa loja foi construída usando uma das nossas soluções da plataforma VTEX. tenha a sua.</h2>
+            <div className="contato-email">
+              <span>Entre em contato</span>
+              <p>comercial@jussi.com.br</p>
+            </div>
+          </div>
         </section>
         <section className="news">
-
+          <div className="container">
+            <h2><span>//</span> receba novidades da nossa área de produtos digitais.</h2>
+            <form>
+              <input type="text" placeholder="Digite seu e-mail" />
+              <input type="submit" value="cadastrar" />
+            </form>
+          </div>
         </section>
         <section className="footer">
 
         </section>
-
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
